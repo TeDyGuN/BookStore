cfc2e71ddbbafe2025de9d5e534db3fdf2a85c65
Ted Carrasco <ted_cc@hotmail.com>
2018-05-07 23:18:28 -0400

    Second Commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,25 +5,8 @@
             <li>Inicio</li>
         </ol>
     </div>
-    <h2 class="text-center">Sistema Administrativo - Fundacion PROFIN</h1>
+    <h2 class="text-center">Sistema de Administracion Libros</h1>
     <div class="col-md-2">
-    </div>
-    <div class="col-md-8">
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          <h3 class="panel-title">Datos</h3>
-        </div>
-        <div class="panel-body">
-
-            <table class="table table-bordered">
-
-              <tr>
-                  <th>Nombre</th>
-                  <td>{{ Auth::user()->name }}</td>
-              </tr>
-            </table>
-        </div>
-      </div>
     </div>
     <div class="col-md-2">
     </div>
